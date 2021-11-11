531d7083828a95196c28e58070778f1c20dc4994
ZeyuWangGit <wzy.franklin@gmail.com>
2021-11-11 21:12:07 +1100

    Distributed cache node

--- glowingcache/http.go
+++ glowingcache/http.go
@@ -2,16 +2,28 @@ package glowingcache
 
 import (
 	"fmt"
+	"glowing-cache/glowingcache/consistenthash"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
 const defaultBasePath = "/_glowingcache/"
+const defaultReplicas = 50
 
 type HTTPPool struct {
-	self     string
-	basePath string
+	self               string
+	basePath           string
+	mu                 sync.Mutex
+	peers              *consistenthash.Map
+	nodeHttpGettersMap map[string]*httpGetter
+}
+
+type httpGetter struct {
+	baseUrl string
 }
 
 func NewHttpPool(self string) *HTTPPool {
@@ -26,7 +38,7 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 }
 
 func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if  !strings.HasPrefix(r.URL.Path, p.basePath) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected request: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
@@ -41,7 +53,7 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	group := GetGroup(groupName)
 	if group == nil {
-		http.Error(w, "Group Not Found" + groupName , http.StatusNotFound)
+		http.Error(w, "Group Not Found"+groupName, http.StatusNotFound)
 		return
 	}
 	view, err := group.Get(key)
@@ -52,3 +64,50 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.nodeHttpGettersMap = make(map[string]*httpGetter)
+	for _, peer := range peers {
+		p.nodeHttpGettersMap[peer] = &httpGetter{
+			baseUrl: peer + p.basePath,
+		}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peerNodeName := p.peers.Get(key); peerNodeName != "" && peerNodeName != p.self {
+		p.Log("Pick peerNodeName %s", peerNodeName)
+		return p.nodeHttpGettersMap[peerNodeName], true
+	}
+	return nil, false
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	peerUrl := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseUrl,
+		url.QueryEscape(group),
+		url.QueryEscape(key))
+	res, err := http.Get(peerUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+var _ PeerGetter = (*httpGetter)(nil)
